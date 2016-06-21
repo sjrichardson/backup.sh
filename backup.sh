f1a8cc3bd40197b1907798519af576ee2129580b
Samuel Richardson <samrichardson@sbcglobal.net>
2016-06-21 19:39:23 -0400

    Add ~/Desktop filepath

--- backup.sh
+++ backup.sh
@@ -3,7 +3,7 @@ OF=backup-$(date +%Y%m%d).tar.gz
 
 DIRECTORY=/Volumes/:dev:null/
 if [ -d "$DIRECTORY" ]; then
-    tar -vczf $DIRECTORY$OF ~/Documents ~/Music ~/Movies ~/Pictures ~/.vimrc ~/.bash_profile 
+    tar -vczf $DIRECTORY$OF ~/Desktop ~/Documents ~/Music ~/Movies ~/Pictures ~/.vimrc ~/.bash_profile 
     clear
     echo backup complete!
 else echo Error: FileBackup is not connected!
