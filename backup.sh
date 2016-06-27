9eb9161d047da337f44bd5ae62fbc25d9c2bf1a9
Samuel Richardson <samrichardson@sbcglobal.net>
2016-06-27 18:03:04 -0400

    Change tar location to :dev:null/Backup

--- backup.sh
+++ backup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 OF=backup-$(date +%Y%m%d).tar.gz
 
-DIRECTORY=/Volumes/:dev:null/
+DIRECTORY=/Volumes/:dev:null/Backup
 if [ -d "$DIRECTORY" ]; then
-    tar -vczf $DIRECTORY$OF ~/Desktop ~/Documents ~/Music ~/Movies ~/Pictures ~/.vimrc ~/.bash_profile 
+    tar -vczf $DIRECTORY$OF ~/Desktop ~/Documents ~/Music ~/Movies ~/Pictures ~/.vimrc ~/.bash_profile --exclude=~/Documents/Dtella/ 
     clear
     echo backup complete!
 else echo Error: FileBackup is not connected!
